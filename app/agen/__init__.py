626a492d8f3c58d0e72930981414ac1593029d7b
M. Ikhsan Pasaribu <mikhsanpasaribu@gmail.com>
2026-01-16 01:09:45 +0700

    Add agent package for LangChain with proposal review implementation

---
+++ app/agen/__init__.py
@@ -0,0 +1,12 @@
+"""
+Paket agen untuk agent LangChain.
+
+Berisi implementasi agent untuk meninjau
+dan mengevaluasi proposal akademik.
+"""
+
+from app.agen.agen_peninjau import AgenPeninjauProposal
+
+__all__ = [
+    "AgenPeninjauProposal",
+]
